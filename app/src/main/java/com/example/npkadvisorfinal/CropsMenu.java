816bb0f03e753b2820f8bfc1cbff30c81d93d4dd
Luis <lfcobom>
2021-09-14 13:37:50 -0500

    Sep142021

--- app/src/main/java/com/example/npkadvisorfinal/CropsMenu.java
+++ app/src/main/java/com/example/npkadvisorfinal/CropsMenu.java
@@ -1,12 +1,16 @@
 package com.example.npkadvisorfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CropsMenu extends Fragment {
+    ImageView buttonadd, buttonmodify, buttondelete, buttonverify;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,13 +57,45 @@ public class CropsMenu extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        }    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crops_menu, container, false);
+        View vista = inflater.inflate(R.layout.fragment_crops_menu, container, false);
+        buttonadd = vista.findViewById(R.id.vercultivos);
+        buttonmodify= vista.findViewById(R.id.reginfo);
+        buttondelete = vista.findViewById(R.id.maps);
+        buttonverify = vista.findViewById(R.id.graphics);
+        buttonadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create new fragment and transaction
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.setReorderingAllowed(true);
+               // Replace whatever is in the fragment_container view with this fragment
+                transaction.replace(R.id.InicialFragment, add_.newInstance("",""));
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+        buttonmodify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create new fragment and transaction
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.setReorderingAllowed(true);
+                // Replace whatever is in the fragment_container view with this fragment
+                transaction.replace(R.id.InicialFragment, Add_Modify.newInstance("",""));
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+
+
+        return vista;
     }
 }
